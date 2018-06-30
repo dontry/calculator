a366a39b4c2da0e72e6ea2d1c976c06c143c4dc9
dontry <mccoy018@gmail.com>
2018-06-30 11:03:50 +1000

    updated component Key  with FlowT ype; updated compnent Keypad;

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-const Key = ({ position, keySymbol, keyValue, keyType }) => {
+type PropTypes = {
+  position: string,
+  keySymbol: string,
+  keyValue: string,
+  keyType: string
+};
+
+const Key = ({ position, keySymbol, keyValue, keyType }: PropTypes) => {
   function handleClick() {
     console.log(keyValue);
   }
-  return <div className={`key ${keyType} ${position}`} onClick={handleClick.bind(this)}>{keySymbol}</div>;
+  return (
+    <div
+      className={`key ${keyType} ${position}`}
+      onClick={handleClick.bind(this)}
+    >
+      {keySymbol}
+    </div>
+  );
 };
 
 export default Key;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,31 +1,40 @@
-import React from 'react'
-import './style.css'
-import Key from './Key'
+import React from "react";
+import "./style.css";
+import Key from "./Key";
+import {
+  TYPE_OPERATOR,
+  TYPE_NUMBER,
+  TYPE_CHANGE_SIGN,
+  TYPE_CLEAR,
+  TYPE_DECIMAL,
+  TYPE_EQUAL,
+  TYPE_PERCENT
+} from "../actions";
 
 const Keypad = () => {
   return (
     <div className="keypad">
-     <Key position="head" keyType="action"  keySymbol="C" value="clear" />
-     <Key position="head" keyType="operator" keySymbol="±" value="+-" />
-     <Key position="head" keyType="action" keySymbol="%" value="%" />
-     <Key position="aside" keyType="operator" keySymbol="÷" value="/" />
-     <Key keyType="number" keySymbol="7" value="7"/>
-     <Key  keyType="number" keySymbol="8" value="8" />
-     <Key  keyType="number" keySymbol="9" value="9" />
-     <Key position="aside" keySymbol="×" value="*" />
-     <Key   keyType="number" keySymbol="4" value="4"/>
-     <Key   keyType="number" keySymbol="5" value="5" />
-     <Key   keyType="number" keySymbol="6" value="6" />
-     <Key position="aside" keyType="number" keySymbol="-" value="-" />
-     <Key  keyType="number" keySymbol="1" value="1" />
-     <Key  keyType="number" keySymbol="2" value="2"/>
-     <Key   keyType="number" keySymbol="3" value="3"/>
-     <Key position="aside"  keySymbol="+" value="+" />
-     <Key position="bottomLeft"  keyType="number" keySymbol="0" value="0" />
-     <Key   keyType="number" keySymbol="." value="."/>
-     <Key position="aside" keyType="number" keySymbol="=" value="="/> 
+      <Key position="head" keyType={TYPE_CLEAR} keySymbol="C" value="clear" />
+      <Key position="head" keyType={TYPE_CHANGE_SIGN} keySymbol="±" value="+/-" />
+      <Key position="head" keyType={TYPE_PERCENT} keySymbol="%" value="%" />
+      <Key position="aside" keyType={TYPE_OPERATOR} keySymbol="÷" value="/" />
+      <Key keyType={TYPE_NUMBER} keySymbol="7" value="7" />
+      <Key keyType={TYPE_NUMBER} keySymbol="8" value="8" />
+      <Key keyType={TYPE_NUMBER} keySymbol="9" value="9" />
+      <Key position="aside" keyTyoe={TYPE_OPERATOR} keySymbol="×" value="*" />
+      <Key keyType={TYPE_NUMBER} keySymbol="4" value="4" />
+      <Key keyType={TYPE_NUMBER} keySymbol="5" value="5" />
+      <Key keyType={TYPE_NUMBER} keySymbol="6" value="6" />
+      <Key position="aside" keyType={TYPE_NUMBER} keySymbol="-" value="-" />
+      <Key keyType={TYPE_NUMBER} keySymbol="1" value="1" />
+      <Key keyType={TYPE_NUMBER} keySymbol="2" value="2" />
+      <Key keyType={TYPE_NUMBER} keySymbol="3" value="3" />
+      <Key position="aside" keyType={TYPE_OPERATOR} keySymbol="+" value="+" />
+      <Key position="bottomLeft" keyType={TYPE_NUMBER} keySymbol="0" value="0" />
+      <Key keyType={TYPE_OPERATOR} keySymbol="." value="." />
+      <Key position="aside" keyType={TYPE_EQUAL} keySymbol="=" value="=" />
     </div>
-  )
-}
+  );
+};
 
-export default Keypad
+export default Keypad;
