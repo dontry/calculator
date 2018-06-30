055d5399e7358a8b46f9252e2610fc78ec4cba36
dontry <mccoy018@gmail.com>
2018-06-30 11:02:34 +1000

    added actions;

---
+++ src/actions/index.js
@@ -0,0 +1,37 @@
+export const TYPE_NUMBER = "NUMBER";
+export const TYPE_OPERATOR = "OPERATOR";
+export const TYPE_CLEAR = "CLEAR";
+export const TYPE_CHANGE_SIGN = "CHANGE_SIGN";
+export const TYPE_DECIMAL = "DECIMAL";
+export const TYPE_EQUAL = "EQUAL";
+export const TYPE_PERCENT = "PERCENT";
+
+export const pressKey = (keyType, keyValue = {}) => {
+  if (keyType === TYPE_OPERATOR) {
+    keyValue = generateOperationFunc(keyValue);
+  }
+  return {
+    type: keyType,
+    payload: keyValue
+  };
+};
+
+function generateOperationFunc(keyValue) {
+  switch (keyValue) {
+    case "+":
+      return;
+    case "-":
+      return b => a => a - b;
+    case "*":
+      return b => a => a * b;
+    case "/":
+      return b => a => a / b;
+    default:
+      return keyValue;
+  }
+}
+
+export const plus = b => a => a + b;
+export const subtract = b => a => a - b;
+export const multiply = b => a => a * b;
+export const divide = b => a / b;
